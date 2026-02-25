a13567b4dbea2619f124ed783c76b40b75a8f053
thanh01pmt <thanh01.pmt@gmail.com>
2026-02-25 21:32:50 +0700

    refactor: Migrate docs to native Next.js/Nextra build process and update Tailwind CSS/PostCSS configuration.

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
 import './globals.css'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
